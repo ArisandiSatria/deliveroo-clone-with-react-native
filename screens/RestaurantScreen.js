6cdf2f9a4cd9090ae5b7cf5ce5621ee0da9ca751
Arisandi <arisandisatria541@gmail.com>
2023-03-27 14:22:18 +0700

    added dishes row in restaurant screen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -9,6 +9,7 @@ import {
   ChevronRightIcon,
 } from "react-native-heroicons/solid";
 import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
+import DishRow from "../components/DishRow";
 
 const RestaurantScreen = () => {
   const {
@@ -82,16 +83,16 @@ const RestaurantScreen = () => {
       <View>
         <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
 
-        {dishes.map((dish) => {
-          <Dishrow
+        {dishes.map((dish) => (
+          <DishRow
             key={dish._id}
             id={dish._id}
             name={dish.name}
-            description={dish.description}
+            description={dish.short_description}
             price={dish.price}
             image={dish.image}
-          />;
-        })}
+          />
+        ))}
       </View>
     </ScrollView>
   );
